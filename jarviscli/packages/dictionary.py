f493fbed144e96efafc5209db7320c3a07a5d035
JoeNNorris <j.norris@hotmail.co.uk>
2018-08-13 09:37:45 -0700

    Fixed dictionary
    In the event of PyDictionary.googlemeaning() not working, PyDictionary.meaning() is employed instead. In the event of PyDictionary.synonym and PyDictionary.antonym not working, neither are displayed.

--- jarviscli/packages/dictionary.py
+++ jarviscli/packages/dictionary.py
@@ -17,18 +17,36 @@ def enablePrint():
 
 
 def dictionary(self):
-    # Returns meaning, synonym and antonym of any word
-    Dict = PyDictionary()
-    print_say('\nEnter word', self)
-    word = input()
-    print('\nMeaning : ' + str(Dict.googlemeaning(word)))
-    blockPrint()
-    syn = Dict.synonym(word)
-    ant = Dict.antonym(word)
-    if syn is not None:
-        syn = [x.encode('UTF8') for x in syn]
-    if ant is not None:
-        ant = [x.encode('UTF8') for x in ant]
-    enablePrint()
-    print('\nSynonyms : ' + str(syn))
-    print('\nAntonyms : ' + str(ant))
+	# Returns meaning, synonym and antonym of any word
+	Dict = PyDictionary()
+	print_say('\nEnter word', self)
+	word = input()
+
+	#Gets and displays each meaning of the word
+	if Dict.googlemeaning(word) == None:
+		#executes if googlemeaning() is not working
+		#part_of_speech refers to verbs, nouns, adverbs etc
+		#definition is the meaning explanantion
+		meaning = Dict.meaning(word)
+		print('\nMeanings: ') 
+		for part_of_speech in meaning:
+			print(part_of_speech + ':')
+			for definition in meaning[part_of_speech]:
+				if '(' in definition:
+					print(' ~' + definition + ')')
+				else:
+					print(' ~' + definition)
+	else:
+		print('\nMeaning : ' + str(Dict.googlemeaning(word)))
+
+	if Dict.synonym(word) != None and Dict.antonym(word) != None:
+		blockPrint()
+		syn = Dict.synonym(word)
+		ant = Dict.antonym(word)
+		syn = [x.encode('UTF8') for x in syn]
+		ant = [x.encode('UTF8') for x in ant]
+		enablePrint()
+		print('\nSynonyms : ' + str(syn))
+		print('\nAntonyms : ' + str(ant))
+	else:
+		print('\nSynonyms and antonyms are not available due to external module error')
